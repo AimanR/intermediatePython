33af7c0bab493995177b90bbe754a9c7cbdb2aa0
Aiman Rafeed <rafeedaiman@gmail.com>
2020-12-16 17:53:58 +0600

    Restrucred 08_Deque.py to make it more readable.

--- 08_Deque.py
+++ 08_Deque.py
@@ -3,43 +3,68 @@
 import collections
 from collections import deque
 
-d = deque("Aiman"); print(d)
+d = deque("Aiman"); 
+print(d)
+print('\n\n')
 
+# append
+
+print('***append***\n')
 d.append('4')
 d.appendleft(5)
 print(d)
+print('\n')
 
-d.pop(); d.pop() 
+
+# pop
+
+print('***pop***\n')
+d.pop();
 print(d)
-
 d.popleft()
 print(d)
+print('\n')
 
+
+# extend
+
+print('***extend***\n')
 d.extend('xxx')
 print(d)
-
 d.extend(['h','e','l','l','o'])
 print(d)
-
 d.extendleft('DDD')
 print(d)
+print('\n')
 
+
+# rotate
+
+print('***rotate***\n')
 e = deque("abcde")
 e.rotate(-1)
 print(e)
-
 e.rotate(1)
 print(e)
-
 e.rotate(7)
 print(e)
 e.rotate(-7)
 print(e)
+print('\n')
 
+
+# maxlen
+
+print('***maxlen***\n')
 f = deque('kjdsd',maxlen=4)
 print(f)
 f.extend('123')
 print(f)
+print('\n')
 
+
+# clear
+
+print('***clear***\n')
 d.clear()
 print(d)
